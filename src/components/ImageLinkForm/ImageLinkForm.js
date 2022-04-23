1c8af0f8597ff21af02886d88f3c3fbd91a528a3
griffinbaker12 <griffin_baker12@icloud.com>
2022-04-22 22:49:39 -0500

    minor edits

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -4,7 +4,7 @@ const ImageLinkForm = ({ message, onInputChange, onButtonSubmit }) => {
   return (
     <div>
       <p className="f3 mt2">{message}</p>
-      <div className="mv3 center">
+      <div className="link-form-container center">
         <div className="center form pa4 br3 shadow-5">
           <input
             className="f4 pa2 w-70 center"
